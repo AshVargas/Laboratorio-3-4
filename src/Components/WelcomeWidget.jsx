4f42e6c76ec1d5a2d3fa2630c9557ea4e8b86d27
AshVargas <vargasashly260@gmail.com>
2025-04-04 21:22:07 -0600

    segundo commit

--- src/Components/WelcomeWidget.jsx
+++ src/Components/WelcomeWidget.jsx
@@ -1,8 +1,13 @@
-export default function WelcomeWidget({email}){
+import { useContext } from "react"
+import { AuthContext } from "../Context/AuthContext"
+
+export default function WelcomeWidget() {
+     
+    const {user} = useContext (AuthContext)
 
     return(
         <>
-           <h2>welcome to our site {email}</h2>
+           <h2>welcome to our site {user.email}</h2>
         </>
     )
 }
---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { useState, createContext } from 'react';
+import { useUserState } from '../Hooks/useUserState';
+
+//1. crear un nuevo contexto
+export const AuthContext = createContext ();
+
+//2 . crear un provider para compartir el estado
+export const AuthProvider = ({children}) => {
+   const [user, setUser] = useState(null);
+   const { login } = useUserState ();
+
+
+   const logout = () => {
+      setUser(null);
+  }
+
+   return (
+    <AuthContext.Provider value={{user, setUser, login, logout}}> 
+       {children}
+    </AuthContext.Provider>
+   );
+}
